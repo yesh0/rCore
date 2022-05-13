efd3fe5d7c7bc1cc419b10202eb30fa58c857fcc
haozixu <haozixu2001@qq.com>
2022-05-13 21:22:39 +0800

    support kretprobe as eBPF tracepoint

--- kernel/src/bpf/tracepoints.rs
+++ kernel/src/bpf/tracepoints.rs
@@ -4,10 +4,10 @@ use alloc::vec::Vec;
 use lazy_static::lazy_static;
 use trapframe::TrapFrame;
 
-use crate::kprobes::{register_kprobe, KProbeArgs};
+use crate::kprobes::{register_kprobe, register_kretprobe, KProbeArgs, KRetProbeArgs};
 use crate::lkm::manager::ModuleManager;
 use crate::sync::SpinLock as Mutex;
-use crate::syscall::{SysError::*, SysResult};
+use crate::syscall::{SysError::{self, *}, SysResult};
 
 use super::{BpfObject::*, *};
 
@@ -21,8 +21,11 @@ pub struct AttachTarget {
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum TracepointType {
     KProbe,
-    KRetProbe,
+    KRetProbeEntry,
+    KRetProbeExit,
 }
+
+use TracepointType::*;
 
 // Current design is very simple and this is only intended for kprobe/kretprobe
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
@@ -31,28 +34,68 @@ pub struct Tracepoint {
     pub token: usize,
 }
 
+impl Tracepoint {
+    pub fn new(tp_type: TracepointType, token: usize) -> Self {
+        Self {
+            tp_type,
+            token,
+        }
+    }
+}
+
 lazy_static! {
     static ref ATTACHED_PROGS: Mutex<BTreeMap<Tracepoint, Vec<Arc<BpfProgram>>>> =
         Mutex::new(BTreeMap::new());
 }
 
-fn kprobe_handler(_tf: &mut TrapFrame, probed_addr: usize) -> isize {
-    let tracepoint = Tracepoint {
-        tp_type: TracepointType::KProbe,
-        token: probed_addr,
-    };
-
+fn run_attached_programs(tracepoint: &Tracepoint) {
     let map = ATTACHED_PROGS.lock();
-    let programs = map.get(&tracepoint).unwrap();
+    let programs = map.get(tracepoint).unwrap();
     for program in programs {
-        let result = program.run();
-        error!("run result: {}", result);
+        let _result = program.run();
+        // error!("run result: {}", result);
     }
+}
+
+fn kprobe_handler(_tf: &mut TrapFrame, probed_addr: usize) -> isize {
+    let tracepoint = Tracepoint::new(KProbe, probed_addr);
+    run_attached_programs(&tracepoint);
+    0
+}
+
+fn kretprobe_entry_handler(_tf: &mut TrapFrame, probed_addr: usize) -> isize {
+    let tracepoint = Tracepoint::new(KRetProbeEntry, probed_addr);
+    run_attached_programs(&tracepoint);
+    0
+}
+
+fn kretprobe_exit_handler(_tf: &mut TrapFrame, probed_addr: usize) -> isize {
+    let tracepoint = Tracepoint::new(KRetProbeExit, probed_addr);
+    run_attached_programs(&tracepoint);
     0
 }
 
 fn resolve_symbol(symbol: &str) -> Option<usize> {
     ModuleManager::with(|mm| mm.resolve_symbol(symbol))
+}
+
+fn parse_tracepoint<'a>(target: &'a str) -> Result<(TracepointType, &'a str), SysError> {
+    let pos = target.find(':').ok_or(EINVAL)?;
+    let type_str = &target[0..pos];
+    let fn_name = &target[(pos + 1)..];
+
+    // determine tracepoint type
+    let tp_type: TracepointType;
+    if type_str.eq_ignore_ascii_case("kprobe") {
+        tp_type = KProbe;
+    } else if type_str.eq_ignore_ascii_case("kretprobe@entry") {
+        tp_type = KRetProbeEntry;
+    } else if type_str.eq_ignore_ascii_case("kretprobe@exit") {
+        tp_type = KRetProbeExit;
+    } else {
+        return Err(EINVAL);
+    }
+    Ok((tp_type, fn_name))
 }
 
 pub fn bpf_program_attach(target: &str, prog_fd: u32) -> SysResult {
@@ -65,30 +108,21 @@ pub fn bpf_program_attach(target: &str, prog_fd: u32) -> SysResult {
         }
     }?;
 
-    let pos = target.find(':').ok_or(EINVAL)?;
-    let type_str = &target[0..pos];
-    let fn_name = &target[(pos + 1)..];
+    let (tp_type, fn_name) = parse_tracepoint(target)?;
+    let addr = resolve_symbol(fn_name).ok_or(ENOENT)?;
+    let tracepoint = Tracepoint::new(tp_type, addr);
 
-    // determine tracepoint type
-    let tp_type: TracepointType;
-    if type_str.eq_ignore_ascii_case("kprobe") {
-        tp_type = TracepointType::KProbe;
+    let mut map = ATTACHED_PROGS.lock();
+    if let Some(programs) = map.get_mut(&tracepoint) {
+        for other_prog in programs.iter() {
+            if Arc::ptr_eq(&program, other_prog) {
+                return Err(EAGAIN);
+            }
+        }
+        programs.push(program);
     } else {
-        return Err(EINVAL);
-    }
-
-    match tp_type {
-        TracepointType::KProbe => {
-            let addr = resolve_symbol(fn_name).ok_or(ENOENT)?;
-            let tracepoint = Tracepoint {
-                tp_type,
-                token: addr,
-            };
-
-            let mut map = ATTACHED_PROGS.lock();
-            if let Some(programs) = map.get_mut(&tracepoint) {
-                programs.push(program);
-            } else {
+        match tp_type {
+            KProbe => {
                 let args = KProbeArgs {
                     pre_handler: Arc::new(kprobe_handler),
                     post_handler: None,
@@ -97,8 +131,25 @@ pub fn bpf_program_attach(target: &str, prog_fd: u32) -> SysResult {
                 let _ = register_kprobe(addr, args).ok_or(EINVAL)?;
                 map.insert(tracepoint, vec![program]);
             }
+            KRetProbeEntry | KRetProbeExit => {
+                let args = KRetProbeArgs {
+                    exit_handler: Arc::new(kretprobe_exit_handler),
+                    entry_handler: Some(Arc::new(kretprobe_entry_handler)),
+                    limit: None,
+                    user_data: addr,
+                };
+                let _ = register_kretprobe(addr, args).ok_or(EINVAL)?;
+                
+                let dual_tp: Tracepoint;
+                if tp_type == KRetProbeEntry {
+                    dual_tp = Tracepoint::new(KRetProbeExit, addr);
+                } else {
+                    dual_tp = Tracepoint::new(KRetProbeEntry, addr);
+                }
+                map.insert(tracepoint, vec![program]);
+                map.insert(dual_tp, vec![]);
+            }
         }
-        _ => todo!(),
     }
     Ok(0)
 }
